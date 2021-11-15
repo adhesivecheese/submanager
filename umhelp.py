d186836af3c943144b9b6b27a147cbea1238546b
GhostofBearBryant <89320003+GhostofBearBryant@users.noreply.github.com>
2021-11-15 17:41:34 -0500

    Update umhelp.py

--- umhelp.py
+++ umhelp.py
@@ -1,6 +1,6 @@
 
 
-# Unmoderated Queue Helper Bot v.7 by u/BuckRowdy.  This is a script I cobbled together from other bots.
+# Unmoderated Queue Helper Bot v.11 by u/BuckRowdy.  This is a script I cobbled together from other bots.
 # The bot manages the unmoderated queue and also checks for highly reported items.
 # The point of the bot is to automate approving posts that fit a certain profile and removing posts that fit the profile of spam.
 
@@ -10,30 +10,41 @@ import sys
 import config
 import praw
 import time
-import datetime2
 from time import localtime, timezone
 from datetime import datetime as dt, timedelta as td, date
+import logging
+
+#Create and configure logger 
+logging.basicConfig(filename="/home/pi/bots/unmod/std.log", 
+                    format='%(asctime)s %(message)s', 
+                    filemode='w') 
+
+#Create an object 
+logger=logging.getLogger() 
+
+#Set the threshold of logger to INFO 
+logger.setLevel(logging.INFO)
 
 # Define the subreddit that you are working on.
 sub_name = 'mod'
 
 
+
 sleep_seconds = 3600     # Number of seconds to sleep between scans (3600 = 1 hour)
-#sleep_seconds = 300     # Five minute timer if needed.
+#sleep_seconds = 600     # Five minute timer if needed instead.
 
 
 #Defines the reddit login function
 def reddit_login():
 
-    print('UnmodHelperBot is starting up - v.8')
+    print('UnmodHelperBot is starting up - v.11')
     print('Connecting to reddit...')
-    print ('    ')
-    print("I\'ll take a look at the unmoderated queue in a second...")
-    print('    ')
+    print("I\'ll take a look at the unmoderated queue in a few seconds...")
+
 
 
     try:
-        reddit = praw.Reddit(   user_agent = 'Unmoderated Queue Helper Bot v.8 by u/buckrowdy',
+        reddit = praw.Reddit(   user_agent = 'Unmoderated Queue Helper Bot v.11 by u/buckrowdy',
                                 username = config.username,
                                 password = config.password,
                                 client_id = config.client_id,
@@ -54,15 +65,20 @@ def printCurrentTime():
 
 
 
+
 def check_modqueue(subreddit):
 
-    print('Checking the reports queue for highly reported items...\n')
-
+    print('Checking the reports queue for highly reported items...')
     reported_items = subreddit.mod.reports(limit=100)
 
     for item in reported_items:
 
-        if item.num_reports >= 5:
+        if item.num_reports >= 2 and item.score <= -12:
+            item.mod.remove()
+            item.mod.lock()
+            logging.info(f'Downvote/Reports user: /u/{item.author} -- {item.subreddit} - {item.permalink}')
+
+        elif item.num_reports >= 5:
             item.mod.remove()
             item.mod.lock()
             item.subreddit.message("I removed something for being highly reported.", f"FYI I removed this item because it got reported 5 times: [{item}](https://reddit.com{item.permalink}) *This was performed by an automated script, please check to ensure this action was correct.*")
@@ -72,17 +88,18 @@ def check_modqueue(subreddit):
             print(f'> Author: {submission.author}\n Title: {submission.title}\n Score: {submission.ups}\n')
             print('        ')
             print(f'Reports: {submission.mod_reports} mod reports, {submission.user_reports} user reports')
+            logging.info(f'Highly Reported: user: /u/{item.author} -- {item.subreddit} - {item.permalink}')
+        else:
+            continue   
 
 
 
 def get_latest_submissions(subreddit):
 
-    print(f'I\'m awake and...\n')
-    print(f'Checking all the posts in the unmoderated queue ...\n')
-
-    submissions = subreddit.mod.unmoderated(limit=300)
+    print(f'Just getting started...')
+    print(f'Checking all the posts in the unmoderated queue ...')
+    submissions = subreddit.mod.unmoderated(limit=1000)
     print('   ')
-
     return submissions
 
 
@@ -115,22 +132,22 @@ def check_submissions(submissions, queue_posts):
             submission.mod.approve()
             continue
 
-        if submission.author == "greenblue98":
+        elif submission.author == "greenblue98":
             submission.mod.approve()
             continue
 
-        if submission.author == "ai0":
+        elif submission.author == "ai0":
             submission.mod.approve()
             continue
 
-        if submission.author == "BuckRowdy":
+        elif submission.author == "BuckRowdy":
             submission.mod.approve()
             continue
 
 
 
         # Remove spam profile posts (reported/downvoted posts). One report could be an automod filter hence greater than 1.
-        if submission.num_reports > 1 and submission.score < 1:
+        elif submission.num_reports > 1 and submission.score < 1:
             r_message = f'Hello u/{submission.author}.\nYour post to r/{submission.subreddit} has been removed. Please review the subreddit [guidelines.](http://reddit.com/r/{submission.subreddit}/about/rules)\n\n'
             n_footer = f"\n\n*If you feel this was done in error, or would like better clarification or need further assistance, please [message the moderators.](https://www.reddit.com/message/compose?to=/r/{submission.subreddit}&subject=Question regarding the removal of this submission by /u/{submission.author}&message=I have a question regarding the removal of this submission: {submission.permalink})*"
             comment_text = r_message + n_footer
@@ -143,60 +160,72 @@ def check_submissions(submissions, queue_posts):
 
             print(f'r/{submission.subreddit}')
             print(f'<!!> Removed reported post by /u/{submission.author} - {submission.title}')
-            print(f'--> https://reddit.com{submission.permalink}')
+            print(f'<!!> https://reddit.com{submission.permalink}')
             print('   ')
             continue
 
 
         # Reports submissions that reach 400 karma within two hours.
-        if submission.created_utc < twohours and submission.user_reports == 0 and submission.score > 400 and not submission.spam and not submission.approved and not submission.removed:
+        elif submission.created_utc < twohours and submission.user_reports == 0 and submission.score >= 400 and not submission.spam and not submission.approved and not submission.removed:
             printCurrentTime()
-            submission.report(reason='<!> This submission is highly upvoted but has not been evaluated.  Please review.')
+            submission.report(reason='<!> This submission is rising quickly but has not been evaluated.  Please review.')
             print('    ')
-            print(f'<!> Reported post - {submission.ups} - /u/{submission.title} - r/{submission.subreddit}.\n')
+            print(f'<!> Reported post - {submission.ups} - /u/{submission.title} - r/{submission.subreddit}.<!>\n')
             continue
 
 
-        # Approve anything in the queue that gets to 500 upvotes with no reports.
-        if submission.user_reports == 0 and submission.score > 499 and not submission.spam and not submission.approved and not submission.removed:
+        # Approve anything in the queue that gets to 600 upvotes with no reports.
+        elif submission.user_reports == 0 and submission.score > 599 and not submission.spam and not submission.approved and not submission.removed:
             submission.mod.approve()
-            print(f'[approved post in {submission.subreddit}]')
-            print(f'Title: {submission.title} - {submission.ups} upvotes - /u/{submission.author} link: {submission.permalink}')
+            print(f'[approved post in {submission.subreddit}] with score of {submission.ups}')
+            print(f'Title: {submission.title} - /u/{submission.author}')
+            print(f'link: {submission.permalink}')
             printCurrentTime()
             print('    ')
             continue
 
 
         # Check if post is older than 18 hours, has at least one upvote, no reports, and approve.
-        if submission.created_utc < eighteen and submission.score > 1 and submission.num_reports == 0 and not submission.spam and not submission.approved and not submission.removed:
+        elif submission.created_utc < eighteen and submission.score > 1 and submission.num_reports == 0 and not submission.spam and not submission.approved and not submission.removed:
             #printCurrentTime()
             submission.mod.approve()
-            #print(f'[Approved Post - 18 hours] - r/{submission.subreddit} - https://reddit.com{submission.permalink}')
-            print(f'[Approved Post - 18 hours] - r/{submission.subreddit}')
+            print(f'[18 hrs Approved Post  - r/{submission.subreddit} |{submission.score}⬆| - u/{submission.author}]')
             continue
 
         # Remove downvoted week old posts to clean up the sub's front page. If a post can't get at least one upvote in a week it doesn't need to be on the sub.
-        if submission.created_utc < week and submission.score <= 0 and submission.num_reports == 0 and not submission.spam and not submission.removed:
+        elif submission.created_utc < week and submission.score <= 0 and submission.num_reports == 0 and not submission.spam and not submission.removed:
             submission.mod.remove(mod_note="week old spam")
             print('    ')
-            print(f'<!> [Removed week old post - with 0 score]')
+            print(f'<!> [Removed week old post - with {submission.score} score] <!>')
             printCurrentTime()
             print(f'r/{submission.subreddit} - /u/{submission.author}')
             print(f'Title: {submission.title}')
-            print(f'->https://reddit.com{submission.permalink}')
+            print(f'https://reddit.com{submission.permalink}')
             print ('    ')
             continue
 
         #  Approve week old posts that don't get upvoted, but don't get downvoted either.
-        if submission.created_utc < week and submission.score == 1 and submission.num_reports == 0 and not submission.spam and not submission.removed:
+        elif submission.created_utc < week and submission.score == 1 and submission.num_reports == 0 and not submission.spam and not submission.removed:
             submission.mod.approve()
 
         else:
             queue_posts.append(submission.id)
 
 
-
     return queue_posts
+
+
+def howManyItems(subreddit):
+
+    submissions = subreddit.mod.unmoderated(limit=1000)
+    reported_items = subreddit.mod.reports(limit=100)
+    num = len(list(reported_items))
+    unmod = len(list(submissions))
+    print('---------------------------------')
+    print(f'There are {num} reported items and {unmod} unmoderated posts remaining.')
+    print('---------------------------------')
+    print('      ')
+
 
 
 #####################
@@ -229,19 +258,23 @@ if __name__ == "__main__":
             submissions = None
             submissions = get_latest_submissions(subreddit)
             check_modqueue(subreddit)
+
         except Exception as e:
             print('\t### ERROR - Could not get posts from reddit')
 
         # If there are posts, start scanning
         if not submissions is None:
 
+
             # Once you have submissions, check valid posts
             queue_posts = check_submissions(submissions, queue_posts)
+            howManyItems(subreddit)
+
 
         # Loop every X seconds (20 minutes)
-        sleep_until = (dt.now() + td(0, sleep_seconds)).strftime('%H:%M:%S')  # Add 0 days, 900 seconds
+        sleep_until = (dt.now() + td(0, sleep_seconds)).strftime('%H:%M:%S')  
         print("\nThat\'s it for now.")
-        print(f'I will be back soon. Sleeping until {sleep_until}') #%Y-%m-%d
+        print(f'I\'ll be back around {sleep_until}.') #%Y-%m-%d
         print('    ')
 
         time.sleep(sleep_seconds)
